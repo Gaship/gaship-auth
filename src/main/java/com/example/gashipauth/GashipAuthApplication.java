c681de2784821eee1e352b96c6eeb0f6fe4e9674
HoFe-U <hochul-y@naver.com>
2022-07-07 09:38:43 +0900

    first 👑 : gaship-auth 프로젝트 생성

---
+++ src/main/java/com/example/gashipauth/GashipAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.gashipauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GashipAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gashipauth/GashipAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gashipauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GashipAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
