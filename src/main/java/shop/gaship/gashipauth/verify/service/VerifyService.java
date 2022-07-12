bbecdd63d9313efb4072c1d3664c256ca4c96bc2
oct-sky-out <kms3335k@naver.com>
2022-07-12 17:01:18 +0900

    [feat✨] : 토큰 생성, 서비스 테스트, 기능 생성

---
+++ src/main/java/shop/gaship/gashipauth/verify/service/VerifyService.java
@@ -0,0 +1,20 @@
+package shop.gaship.gashipauth.verify.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : shop.gaship.gashipauth.verify.service <br/>
+ * fileName       : VerifyService <br/>
+ * author         : 김민수 <br/>
+ * date           : 2022/07/12 <br/>
+ * description    : <br/>
+ * ===========================================================  <br/>
+ * DATE              AUTHOR             NOTE                    <br/>
+ * -----------------------------------------------------------  <br/>
+ * 2022/07/12           김민수               최초 생성                         <br/>
+ */
+@Service
+public class VerifyService {
+
+//    public void
+}
