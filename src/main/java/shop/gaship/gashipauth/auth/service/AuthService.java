3c7b7c402702fb2e5512be82b21a8ea8e4a8c07a
조재철 <rhsnqk@gmail.com>
2022-08-11 13:26:41 +0900

    [fix🚑] : logout, issueToken 관련 반환 타입 수정

--- src/main/java/shop/gaship/gashipauth/auth/service/AuthService.java
+++ src/main/java/shop/gaship/gashipauth/auth/service/AuthService.java
@@ -1,7 +1,9 @@
 package shop.gaship.gashipauth.auth.service;
 
 import org.springframework.http.ResponseEntity;
+import shop.gaship.gashipauth.token.dto.JwtTokenDto;
 import shop.gaship.gashipauth.token.dto.request.UserInfoForJwtRequestDto;
+import shop.gaship.gashipauth.token.dto.response.JwtResponseDto;
 
 /**
  * The interface Auth service.
@@ -19,7 +21,7 @@ public interface AuthService {
      * @param memberNo 회원 번호.
      * @return 로그아웃에 대한 응답 정보.
      */
-    ResponseEntity<?> logout(String accessToken, String refreshToken, Integer memberNo);
+    void logout(String accessToken, String refreshToken, Integer memberNo);
 
     /**
      * Jwt 발급 받는 비즈니스 로직을 처리하는 메서드.
@@ -27,5 +29,5 @@ public interface AuthService {
      * @param userInfoDto 회원 정보(회원 번호, 권한).
      * @return 토큰 재발급에 대한 응답 정보.
      */
-    ResponseEntity<?> issueJwt(UserInfoForJwtRequestDto userInfoDto);
+    JwtResponseDto issueJwt(UserInfoForJwtRequestDto userInfoDto);
 }
